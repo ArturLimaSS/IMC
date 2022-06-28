d755c4eceafb9460f16f67d5a3b2205bd6244f4c
Artur Lima <85823067+ArturLimaSS@users.noreply.github.com>
2022-06-28 15:31:05 -0300

    Update Style and Script

--- script.js
+++ script.js
@@ -1,22 +1,49 @@
 function calculate() {
-  var alturaVar = document.getElementById('altura').value
-  var pesoVar = document.getElementById('peso_input').value
-  let calc = (pesoVar / (alturaVar * alturaVar)) * 10000
-  let element = document.getElementById('dado')
-  if (isNaN(calc)) {
-    element.innerHTML = 'Digite os valores acima!'
-  } else {
-    element.innerHTML = 'Seu IMC é: ' + calc.toFixed(2)
-    if (calc < 18.5) {
-      document.getElementById('magreza').style.backgroundColor = 'purple'
-    } else if (calc > 18.5 && calc <= 25) {
-      document.getElementById('normal').style.backgroundColor = 'purple'
-    } else if (calc > 25 && calc <= 30) {
-      document.getElementById('sobrepeso').style.backgroundColor = 'purple'
-    } else if (calc > 30 && calc <= 35) {
-      document.getElementById('obesidade').style.backgroundColor = 'purple'
-    } else if (calc > 35 && calc <= 40) {
-      document.getElementById('obesidadeGrave').style.backgroundColor = 'purple'
+    var alturaVar = document.getElementById('altura').value
+    var pesoVar = document.getElementById('peso_input').value
+    let calc = (pesoVar / (alturaVar * alturaVar)) * 10000
+    let element = document.getElementById('dado')
+
+
+    if (isNaN(calc)) {
+        element.innerHTML = 'Digite os valores acima!'
+        element.style.display = "block"
+
+
+    } else {
+        element.innerHTML = 'Seu IMC é: ' + calc.toFixed(2)
+        element.style.display = "block"
+        if (calc < 18.5) {
+            document.getElementById("magreza").style.backgroundColor = "rgb(210, 228, 209)"
+            document.getElementById("normal").style.backgroundColor = "white"
+            document.getElementById("sobrepeso").style.backgroundColor = "white"
+            document.getElementById("obesidade").style.backgroundColor = "white"
+            document.getElementById("obesidadeGrave").style.backgroundColor = "white"
+        } else if (calc > 18.5 && calc < 24.9) {
+            document.getElementById("magreza").style.backgroundColor = "white"
+            document.getElementById("normal").style.backgroundColor = "rgb(210, 228, 209)"
+            document.getElementById("sobrepeso").style.backgroundColor = "white"
+            document.getElementById("obesidade").style.backgroundColor = "white"
+            document.getElementById("obesidadeGrave").style.backgroundColor = "white"
+        } else if (calc > 25 && calc < 29.9) {
+            document.getElementById("magreza").style.backgroundColor = "white"
+            document.getElementById("normal").style.backgroundColor = "white"
+            document.getElementById("sobrepeso").style.backgroundColor = "rgb(210, 228, 209)"
+            document.getElementById("obesidade").style.backgroundColor = "white"
+            document.getElementById("obesidadeGrave").style.backgroundColor = "white"
+        } else if (calc > 30 && calc < 39.9) {
+            document.getElementById("magreza").style.backgroundColor = "white"
+            document.getElementById("normal").style.backgroundColor = "white"
+            document.getElementById("sobrepeso").style.backgroundColor = "white"
+            document.getElementById("obesidade").style.backgroundColor = "rgb(210, 228, 209)"
+            document.getElementById("obesidadeGrave").style.backgroundColor = "white"
+        } else if (calc > 40) {
+            document.getElementById("magreza").style.backgroundColor = "white"
+            document.getElementById("normal").style.backgroundColor = "white"
+            document.getElementById("sobrepeso").style.backgroundColor = "white"
+            document.getElementById("obesidade").style.backgroundColor = "white"
+            document.getElementById("obesidadeGrave").style.backgroundColor = "rgb(210, 228, 209)"
+        }
+
     }
-  }
 }
